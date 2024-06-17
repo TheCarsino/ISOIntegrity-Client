1ac540bcbf9bc58bb66a4edfc0da66eb27c263f2
Lucas Podestá <78005968+TheCarsino@users.noreply.github.com>
2024-06-17 00:20:11 -0500

    Sixth Iteration v2.2
    Deploy changes

---
+++ pm2.config.cjs
@@ -0,0 +1,11 @@
+{
+  apps: [
+    {
+      name: "client", // Replace with your application name
+      script: "npm run dev", // Replace with your actual start script
+      env: {
+        // Define environment variables here if needed
+      },
+    },
+  ],
+};
